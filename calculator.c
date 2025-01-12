42228bb7fbf453b1ec41c0fef216d48ab7134878
mattfraserlong <mattfraserlong@gmail.com>
2025-01-12 13:57:08 +0000

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+/* ###########Create a simple calculator###########*/
+/* Default display is zero*/
+/*Enter number (float) or operator (string)*/
+/*Display previous step*/
+/*When hitting enter:*/
+/*If float, display float*/
+/*If float, then operator, return value of expression (float/operator/float)*/
+/*Either evaluate or post error message*/
+
+int number;
+char operator;
+int answer;
+
+int calculator (int a, int b)  {
+
+
+
+};
+
+int main () {
+
+    initscr(); 
+    printw("########### C-alculator ############\n")
+    getstr(a);
+
+
+
+
+}
